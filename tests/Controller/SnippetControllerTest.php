37e3e647fcf511d1b0c8f4308242f06721d65578
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2011-10-22 22:28:23 +0200

    Birth of the list action.

---
+++ tests/Controller/SnippetControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__.'/../bootstrap.php';
+
+use Silex\WebTestCase;
+
+class SnippetControllerTest extends WebTestCase
+{
+    public function createApplication()
+    {
+        return require __DIR__.'/../../src/app.php';
+    }
+
+    public function testIndex()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/snippet');
+
+        $this->assertTrue($client->getResponse()->isOk());
+        $this->assertEquals(3, $crawler->filter('ul li')->count());
+    }
+}
